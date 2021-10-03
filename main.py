8f33a682bc49ffd823863c6656202196fccc30dc
x0y14 <ya01ma08@gmail.com>
2021-10-03 17:36:45 +0900

    add Vm.pc, Vm.start

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from vm import VM
 def main():
     vm = VM()
     vm.state()
+    vm.start()
 
 
 if __name__ == '__main__':
--- vm.py
+++ vm.py
@@ -1,18 +1,43 @@
+import time
+
 from misc import *
 
 
 class VM:
     def __init__(self):
+        # program counter
+        self.pc = 0
+
+        # register
         self.reg_a = 0
         self.reg_b = 0
         self.reg_c = 0
+
+        # memory
         self.mem = [
             "set_reg_a", 1,
             "set_reg_a", 0,
+            "exit",
         ]
 
     def state(self):
-        print(f"VM {{ reg_a: {self.reg_a}, reg_b: {self.reg_b}, reg_c: {self.reg_c}, mem: {self.mem} }}")
+        print(f"VM {{ reg_a: {self.reg_a}, reg_b: {self.reg_b}, reg_c: {self.reg_c}, mem: {self.mem} }}, pc: {self.pc}")
+
+    def start(self):
+        while True:
+            op = self.mem[self.pc]
+            if op == "set_reg_a":
+                n = self.mem[self.pc+1]
+                self.reg_a = n
+                self.pc += 2
+            elif op == "exit":
+                print("VM exit")
+                break
+            else:
+                raise f"Unknown operator({op})"
+
+            self.state()
+            time.sleep(0.5)
 
     def set_mem(self, addr, n):
         self.mem[addr] = n
