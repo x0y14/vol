5962f42b65567652cc2fb314d026b2d24faaf546
x0y14 <ya01ma08@gmail.com>
2021-10-05 20:37:01 +0900

    コンパイラとvmを使用可能にし、実行可能にした

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import sys
 from vm import VM
+from compiler import Compiler
 
 
 def main():
     filepath = sys.argv[1]
-    # todo : compiler
+    asm_path = sys.argv[2]
+    cpr = Compiler(filepath)
+    cpr.convert()
+    cpr.mem_mapping()
+    code = cpr.codegen()
+    with open(asm_path, "w") as f:
+        f.write(code)
 
     # exec
-    vm = VM(filepath)
+    vm = VM(asm_path)
     vm.start()
 
 
