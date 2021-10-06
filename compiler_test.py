2c5b2ff4010fe32666b3125617cb97aeebbcd441
x0y14 <ya01ma08@gmail.com>
2021-10-06 09:56:31 +0900

    スタックのポインタの位置が変数で取得できるかと思ったけどできなかったテスト

--- compiler_test.py
+++ compiler_test.py
@@ -1,9 +1,18 @@
 import unittest
 
+class A:
+    def __init__(self):
+        self.mem = []
+        self.mp = len(self.mem) - 1
+
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        # self.assertEqual(True, False)  # add assertion here
+        a = A()
+        self.assertEqual(-1, a.mp)
+        a.mem.append(1)
+        self.assertEqual(0, a.mp)
 
 
 if __name__ == '__main__':
