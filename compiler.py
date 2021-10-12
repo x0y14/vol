f1e4f0b18b0a1918deae9a33312d2920a11f3787
x0y14 <ya01ma08@gmail.com>
2021-10-12 18:02:19 +0900

    引数、代入でアドレス変換に対応。 読みやすく

--- compiler.py
+++ compiler.py
@@ -5,10 +5,10 @@ from pprint import pprint
 
 class Compiler:
     def __init__(self):
-        pass
+        self.defined_variables = []
 
     def codegen(self, tree):
-        asm_lines = ["call main", "exit", ""]
+        asm_lines = ["call main", "exit"]
 
         head, rest = tree[0], tree[1:]
         if head != "stmts":
@@ -29,28 +29,118 @@ class Compiler:
 
         return asm_lines
 
+    def exchange_defined_variable(self, name_or_value):
+        if name_or_value not in self.defined_variables:
+            return name_or_value
+
+        pos = self.defined_variables.index(name_or_value) + 1
+        return f"[bp-{pos}]"
+
     def cdg_func_def(self, rest):
         asm_lines = []
 
         func_name = rest[0]
-        # func_values = tree[1]?
-        func_body = rest[2]
 
-        asm_lines.append(f"{func_name}:")
+        func_args = rest[1]
+
+        func_body = rest[2]
+        asm_lines.append("")
+        asm_lines.append(f"\n{func_name}:")
+        asm_lines.append(f"\t# 呼び出し元に戻れるように現状保存")
         asm_lines.append("\tpush bp")
         asm_lines.append(f"\tcp sp bp")
+
+        if len(func_args) != 0:
+            asm_lines.append(f"\n\t# 引数をローカル変数として代入")
+        arg_no = 2
+        for arg in func_args:
+            # 引数をローカル変数へと変換。
+            self.defined_variables.append(arg)
+            arg_as_local_value_pos = self.defined_variables.index(arg) + 1
+            asm_lines.append(f"\t# {arg}")
+            asm_lines.append(f"\tsub_sp 1")
+            asm_lines.append(f"\tcp [bp+{arg_no}] [bp-{arg_as_local_value_pos}]")
+            arg_no += 1
 
         asm_lines.append(f"\t# todo : 関数本体")
         for stmt in func_body:
             stmt_head, stmt_rest = stmt[0], stmt[1:]
+            asm_lines.append("")
 
             if stmt_head == "call":
                 func_name = stmt_rest[0]
-                # func_args = stmt_rest[1]
-                asm_lines.append(f"\tcall {func_name}")
+                func_args: list = stmt_rest[1]
+                if type(func_args) is list and len(func_args) != 0:
+                    # has args; 逆順にpush
+                    func_args.reverse()
+                    for arg in func_args:
+                        # 変数を変換して入れる
+                        arg_candidate = self.exchange_defined_variable(arg)
+                        if arg_candidate is None:
+                            asm_lines.append(f"\tpush {arg} # 引数")
+                        else:
+                            asm_lines.append(f"\tpush {arg_candidate} # 引数({arg})")
+
+                    asm_lines.append(f"\tcall {func_name} # 関数呼び出し")
+                    # sp戻す。
+                    asm_lines.append(f"\tadd_sp {len(func_args)} # 呼び出し後の位置修正")
+
+                else:
+                    asm_lines.append(f"\tcall {func_name} # 関数呼び出し")
+            elif stmt_head == "var":
+                self.defined_variables.append(stmt_rest[0])
+                asm_lines.append(f"\t# 引数準備 : {stmt_rest[0]}")
+                asm_lines.append(f"\tsub_sp 1")
+
+            elif stmt_head == "set":
+                # print(stmt_rest)
+                value_def_name = stmt_rest[0]  # name
+                value_data = stmt_rest[1]  # value
+
+                asm_lines.append(f"\t# ローカル変数に値を代入")
+
+                # value = self.exchange_defined_variable(value_def_name)
+                # if value is None:
+                #     value = value_data
+
+                # int
+                if type(value_data) is int:
+                    value = value_data
+                    pos = self.defined_variables.index(value_def_name)+1
+                    asm_lines.append(f"\tcp {value} [bp-{pos}]")
+                # str
+                elif (type(value_data) is str) and (value_data[0] == "\"" and value_data[-1] == "\""):
+                    value = value_data
+                    pos = self.defined_variables.index(value_def_name)+1
+                    asm_lines.append(f"\tcp {value} [bp-{pos}]")
+                # defined keyword
+                elif value_data in self.defined_variables:
+                    pos = self.defined_variables.index(value_def_name)+1
+                    value = self.exchange_defined_variable(value_data)
+                    asm_lines.append(f"\tcp {value} [bp-{pos}]")
+                else:
+                    raise Exception(f"not yet impl: {stmt_rest}")
+
+            elif stmt_head == "print":
+                value_or_keyword = stmt_rest[0]
+
+                if type(value_or_keyword) is int:
+                    # this is int
+                    asm_lines.append(f"\techo {value_or_keyword}")
+                elif type(value_or_keyword) is str and (value_or_keyword[0] == "\"" and value_or_keyword[-1] == "\""):
+                    # this is str
+                    asm_lines.append(f"\techo {value_or_keyword}")
+                elif type(value_or_keyword) is str and (value_or_keyword in self.defined_variables):
+                    # this is keyword
+                    pos = self.defined_variables.index(value_or_keyword)+1
+                    asm_lines.append(f"\techo [bp-{pos}]")
+                else:
+                    raise Exception(f"not yet impl: {stmt_rest}")
+
             else:
                 raise Exception(f"not yet impl: {stmt_head}")
 
+        asm_lines.append(f"\n\t# 現状復帰")
         asm_lines.append(f"\tcp bp sp")
         asm_lines.append(f"\tpop bp")
         asm_lines.append(f"\tret")
@@ -80,4 +170,3 @@ if __name__ == "__main__":
     asm = cpr.codegen(t)
 
     export(asm, output_path)
-
